cb9160250e6ecc519620f5978752a497925c15c3
farpa <arpirez.fabio@gmail.com>
2021-03-13 12:26:00 -0300

    PAC-10_implement_unit_testing

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+      _id: "507f191e810c19729de860ea",
+      name: "Fabio",
+      username: "Fabio03",
+      password: "mystrongPassword"
+    },
+    users: [
+      {
+        _id: "507f191e810c19729de860ea",
+        name: "Fabio",
+        username: "Fabio03",
+        password: "mystrongPassword"
+      },
+      {
+        _id: "507f191e810c19729de860eb",
+        name: "Marlen",
+        username: "Marlen_34",
+        password: "mystrongPassword"
+      }
+    ]
+  };
